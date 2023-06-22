9ea00e34a4c5a296e99fd2421a56428435b32890
dsalinux2023 <c00387299@gmail.com>
2023-06-22 09:36:57 +0530

    setjump and longjump

--- june/1_setjmp.c
+++ june/1_setjmp.c
@@ -1,1 +1,16 @@
-
+#include<stdio.h>
+#include<stdlib.h>
+#include <setjmp.h>
+static jmp_buf buf;
+int main()
+{
+    volatile int b;
+    b=3;
+    if(setjmp(buf) != 0){
+        printf("%d ", b);
+        exit(0);
+    }
+    b=5;
+    longjmp(buf,1);
+    return 0;
+}
